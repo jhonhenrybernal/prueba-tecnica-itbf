75c38f3c87f982cf2282c19d9f649d129073c371
Jhon <jhonhenrybernal@gmail.com>
2022-06-17 23:00:09 -0500

    Parametrizacion migraciones

--- database/migrations/2022_06_17_213355_create_hoteles_table.php
+++ database/migrations/2022_06_17_213355_create_hoteles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateHotelesTable extends Migration
 {
@@ -15,6 +16,11 @@ class CreateHotelesTable extends Migration
     {
         Schema::create('hoteles', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre')->nullable();
+            $table->string('ciudad')->nullable();
+            $table->integer('num-habitantes')->nullable();
+            $table->string('direccion')->nullable();
+            $table->integer('nit')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_06_17_213613_create_habitacions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabitacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habitacions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cantidad')->nullable();
+            $table->string('tipo')->nullable();
+            $table->string('acomodacion')->nullable();
+            $table->bigInteger('hotel_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habitacions');
+    }
+}
